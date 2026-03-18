7efc2150e4742eafa5539670842cb814c99511cf
Vijayalakshmi-Bharatesh30 <vijayalakshmi.cs25@bmsce.ac.in>
2026-03-18 10:45:08 +0530

    Add files via upload

---
+++ Multilevel queue Scheduling.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, time = 0, completed = 0;
+    int at[20], bt[20], ct[20], tat[20], wt[20], type[20], done[20];
+
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        printf("Enter AT BT Type(1:System, 2:User) for P%d: ",i+1);
+        scanf("%d%d%d",&at[i],&bt[i],&type[i]);
+        done[i] = 0;
+    }
+
+    while(completed < n)
+    {
+        int idx = -1;
+        int min_at = 9999;
+
+        for(i=0;i<n;i++)
+        {
+            if(type[i]==1 && done[i]==0 && at[i]<=time)
+            {
+                if(at[i] < min_at)
+                {
+                    min_at = at[i];
+                    idx = i;
+                }
+            }
+        }
+
+        if(idx != -1)
+        {
+            time += bt[idx];
+
+            ct[idx] = time;
+            tat[idx] = ct[idx] - at[idx];
+            wt[idx] = tat[idx] - bt[idx];
+
+            done[idx] = 1;
+            completed++;
+            continue;
+        }
+
+        min_at = 9999;
+        idx = -1;
+
+        for(i=0;i<n;i++)
+        {
+            if(type[i]==2 && done[i]==0 && at[i]<=time)
+            {
+                if(at[i] < min_at)
+                {
+                    min_at = at[i];
+                    idx = i;
+                }
+            }
+        }
+
+        if(idx != -1)
+        {
+            time += bt[idx];
+
+            ct[idx] = time;
+            tat[idx] = ct[idx] - at[idx];
+            wt[idx] = tat[idx] - bt[idx];
+
+            done[idx] = 1;
+            completed++;
+        }
+        else
+        {
+            time++;
+        }
+    }
+
+    float avg_wt = 0, avg_tat = 0;
+
+    printf("\nPID\tAT\tBT\tType\tCT\tTAT\tWT\n");
+
+    for(i=0;i<n;i++)
+    {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+        i+1,at[i],bt[i],type[i],ct[i],tat[i],wt[i]);
+
+        avg_wt += wt[i];
+        avg_tat += tat[i];
+    }
+
+    printf("\nAverage WT = %.2f",avg_wt/n);
+    printf("\nAverage TAT = %.2f\n",avg_tat/n);
+
+    return 0;
+}
